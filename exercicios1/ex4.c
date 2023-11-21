29f635e4a21107005be776ce67662676cfd7e487
Leonel <leonelfhernandez@outlook.com>
2023-11-21 13:27:52 -0300

    Adição Comentários

--- exercicios1/ex4.c
+++ exercicios1/ex4.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 int main(){
-    float altura, peso, imc;
+    float altura, peso, imc; //Definição das variáveis
     printf("Vamos calcular seu IMC!\n");
-    printf("Digite a sua altura em metros: ");
+    printf("Digite a sua altura em metros: "); //Entrada altura
     scanf("%f", &altura);
-    printf("Digite o seu peso em KG: ");
+    printf("Digite o seu peso em KG: "); //Entrada peso
     scanf("%f", &peso);
-    imc = peso / (altura * altura);
-    printf("O seu IMC é de %.1f KG/m²", imc);
+    imc = peso / (altura * altura);  //Cálculo IMC
+    printf("O seu IMC é de %.1f KG/m²", imc);  //IMC
     return 0;
 }
