f321994a721598d5d715afc1140055611a12cb64
Leonel <leonelfhernandez@outlook.com>
2023-11-21 14:05:32 -0300

    Verificação de múltiplos

--- exercicios2/ex1.c
+++ exercicios2/ex1.c
@@ -2,15 +2,15 @@
 
 int main(){
     int n1,n2;
-    printf("Digite seu primeiro numero: ");
+    printf("Digite seu primeiro numero: "); //Entrada número 1
     scanf("%d", &n1);
-    printf("Digite seu segundo numero: ");
+    printf("Digite seu segundo numero: "); //Entrada número 2
     scanf(" %d", &n2);
     if (n1 % n2 == 0){
-        printf("%d e multiplo de %d", n1,n2);
+        printf("%d e multiplo de %d", n1,n2); //Caso seja múltiplo
     }
     else{
-        printf("%d nao e multiplo de %d", n1,n2);
+        printf("%d nao e multiplo de %d", n1,n2); //Caso não seja múltiplo
     }
     return 0;
 }
