21cdf78d8d94318838b936c4041af7078ead9c20
Leonel Hernandez <leonelfhernandez@outlook.com>
2024-08-02 23:55:15 -0300

    alteração pastas semana 2 e 3 em uma só

---
+++ Semana4/Exercicios4/ex001.c
@@ -0,0 +1,11 @@
+/*Faça um programa que exibe todos os números de 0 até 10, inclusive o 10*/
+
+#include <stdio.h>
+
+int main(){
+    //Contador para fazer o loop
+    for(int i = 0; i <= 10; i++){
+        printf("%d\n", i);      //Saída de 0 a 10
+    }
+    return 0;
+}
